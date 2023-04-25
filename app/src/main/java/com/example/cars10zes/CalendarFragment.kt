3552651ae8df434f8f44a30daf3c29b18a8c8722
Miguel Mauer <73600075+MMaue@users.noreply.github.com>
2023-04-25 15:51:18 +0200

    Update CalendarFragment.kt

--- app/src/main/java/com/example/cars10zes/CalendarFragment.kt
+++ app/src/main/java/com/example/cars10zes/CalendarFragment.kt
@@ -1,59 +1,88 @@
 package com.example.cars10zes
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+    private lateinit var textCalendarMonth: TextView
+    private lateinit var buttonCalendarPrevMonth: ImageButton
+    private lateinit var buttonCalendarNextMonth: ImageButton
+    private lateinit var spinnerProjects: Spinner
+    private lateinit var timeTracking: TimeTracking
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(
+        view: View,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        this.activity?.title = getString(R.string.title_calendar)
+
+        timeTracking = TimeTracking(requireActivity().applicationContext)
+        timeTracking.selectedProject = timeTracking.getLastProject()
+
+        buttonCalendarPrevMonth = view.findViewById(R.id.button_calendar_prev_month)
+        buttonCalendarNextMonth = view.findViewById(R.id.button_calendar_next_month)
+        textCalendarMonth = view.findViewById(R.id.text_calendar_month)
+
+        buttonCalendarPrevMonth.setOnClickListener {
+            timeTracking.selectedDate = timeTracking.selectedDate.minusMonths(1)
+            setMonthView()
+        }
+
+        buttonCalendarNextMonth.setOnClickListener {
+            timeTracking.selectedDate = timeTracking.selectedDate.plusMonths(1)
+            setMonthView()
+        }
+
+        spinnerProjects = view.findViewById(R.id.spinner_cal_project)
+        val adapter = this.context?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_spinner_dropdown_item,
+                timeTracking.getProjectsList())
+        }
+        adapter?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerProjects.adapter = adapter
+
+
+        spinnerProjects.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                timeTracking.selectedProject = adapterView?.getItemAtPosition(position).toString()
+                setMonthView()
+            }
+        }
+
+        setMonthView()
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setMonthView() {
+        textCalendarMonth.text = timeTracking.getCalendarDate()
+
+        val daysOfMonth = timeTracking.getDaysOfMonth()
+        val adapter = CalendarAdapter(daysOfMonth)
+
+        val recyclerCalendar = view?.findViewById<RecyclerView>(R.id.recyclerviewCalendar)
+        if (recyclerCalendar != null) {
+            recyclerCalendar.adapter = adapter
+            recyclerCalendar.layoutManager = GridLayoutManager(context, 7)
+        }
     }
 }
